05528e903c4c49df3582f64d2d2d7b42d5bb6a91
sunnioz <ChienNV.B20VT063@stu.ptit.edu.vn>
2023-11-05 00:57:58 +0700

    them chuc nang add to pl

--- db.py
+++ db.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template,redirect,url_for,flash,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_login import UserMixin
 from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
-
 from flask_admin import Admin
 from flask import abort
 from flask_login import current_user
-from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib.sqla import ModelView
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Length,EqualTo,Email,DataRequired,ValidationError
 
+from flask_login import login_user,logout_user,login_required
+from flask_mail import Message
+
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
@@ -112,18 +117,161 @@ class UserView(ModelView):
 
 
 class PlaylistView(Admin_Controll):
-    form_columns = [
-        'user_id',
-        'song_id',
-    ]
     list_columns = [
         'user_id',
-        'song_id',
+        'id_track',
+    ]
+
+class TrackView(Admin_Controll):
+    list_columns = [
+        'id_track',
+        'title',
+        'img'
     ]
 
 admin.add_view(UserView(User,db.session))
-admin.add_view(ModelView(Tracks,db.session))
-admin.add_view(ModelView(Playlist,db.session))
+admin.add_view(TrackView(Tracks,db.session))
+admin.add_view(PlaylistView(Playlist,db.session))
 admin.add_view(ModelView(Albums,db.session))
 
-app.run(debug=True)
+
+class RegisterForm(FlaskForm):
+
+    def validate_username(self,username_to_check):
+        user = User.query.filter_by(username = username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exits! Please try another username')
+
+    def validate_email_address(self,email_to_check):
+        email = User.query.filter_by(email_address = email_to_check.data).first()
+        if email:
+            raise ValidationError('Email already exits! Please try another Email')
+
+    username = StringField(label='User Name:',validators=[Length(min=2,max=30),DataRequired()])
+    email_address = StringField(label='EMAIL ADDRESS:',validators=[Email(),DataRequired()])
+    password1 = PasswordField(label='ENTER YOUR PASSWORD:',validators=[Length(min=6),DataRequired()])
+    password2 = PasswordField(label='VERIFY:',validators=[EqualTo('password1'),DataRequired()])
+    submit = SubmitField(label='Create ACCOUNT')
+   
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name ',validators=[DataRequired()])
+    password = PasswordField(label='PassWord ',validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
+
+class ResetRequestForm(FlaskForm):
+    email_address = StringField(label='Email Address',validators=[DataRequired()])
+    submit = SubmitField(label='Reset Password')
+
+class ResetPasswordForm(FlaskForm):
+    password1 = PasswordField(label='ENTER YOUR PASSWORD:',validators=[Length(min=6),DataRequired()])
+    password2 = PasswordField(label='VERIFY:',validators=[EqualTo('password1'),DataRequired()])
+    submit = SubmitField(label='ChangePassword')
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/register',methods = ['GET','POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username = form.username.data,
+                              email_address = form.email_address.data,
+                              password = form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        flash('You just created successfully',category='success')
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}',category='danger')
+    return render_template('register.html',form = form)
+
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username = form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password = form.password.data
+        ):
+            login_user(attempted_user)
+            if attempted_user.username =='admin':
+                return redirect('/admin')
+            flash(f'Success! You just logged in as {attempted_user.username}',category='success')
+            return redirect(url_for('getsongs'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+    return render_template('login.html',form = form)
+@app.route('/logout',methods = ['GET','POST'])
+def logout():
+    logout_user()
+    flash('You have been loged out',category='success')
+    return redirect(url_for('home_page'))
+
+def send_mail(user):
+    token = user.get_token()
+    msg = Message('Password Reset Request',recipients=[user.email_address],sender='ChienNV.B20VT063@stu.ptit.edu.vn')
+    msg.body = f''' to reset your password. Please follow the link below
+
+    {url_for('reset_token',token = token,_external = True)}
+
+    If you didn't send a password reset request. Please ignore this message.
+
+ '''
+    mail.send(msg)
+
+@app.route('/reset_password',methods = ['POST','GET'])
+def reset_request():
+    form = ResetRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email_address =form.email_address.data).first()
+        if user:
+            send_mail(user)
+            flash('Reset request sent. Check your mail')
+            return redirect(url_for('login_page'))
+        else:
+            flash('Email is not exist')
+    return render_template('reset_request.html',form = form)
+
+@app.route('/reset_password/<token>',methods = ['GET','POST'])
+def reset_token(token):
+    user = User.verify_token(token)
+    if user is None:
+        flash('That is invalid token or expired.Please try again')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hash_password = bcrypt.generate_password_hash(form.password1.data).decode('utf-8')
+        user.password_hash = hash_password
+        db.session.commit()
+        flash('Password changed!')
+        return redirect(url_for('login_page'))
+    return render_template('changepass.html',form = form)
+
+@app.route('/changepassword',methods = ['GET','POST'])
+def changepass():
+    
+    pass
+
+@app.route('/getsongs',methods = ['GET'])
+@login_required
+def getsongs():
+    songs = random.sample(Tracks.query.all(),5)
+    return render_template('home.html',songs = songs)
+
+@app.route('/addtopl',methods = ['GET','POST'])
+def addtopl():
+    if request.method == 'POST':
+        song_id = request.form['song_id']
+        user_id = current_user.get_id()
+
+        usersong = Playlist(user_id = user_id,id_track = song_id)
+        db.session.add(usersong)
+        db.session.commit()
+        flash('Successfully',category='success')
+    return render_template('home.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
