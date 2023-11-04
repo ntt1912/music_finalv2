1e89ffaa1ebd5874389cc02f340e56d3bf6eecaa
ntt1912 <124802515+ntt1912@users.noreply.github.com>
2023-11-04 23:18:56 +0700

    new commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\HocVienPtit\Tai_lieu_ki_7\BTL\Music_final\env
+set VIRTUAL_ENV=D:\New folder\music_final\env
 
 if not defined PROMPT set PROMPT=$P$G
 
